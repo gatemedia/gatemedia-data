5071a1885331fafb5be4bafec8ae2b8ee067b8cd
Mike Andrzejewski <mike.aski@gatemedia.ch>
2013-06-12 18:31:24 +0200

    Improve tests runner

--- ember-data.gemspec
+++ ember-data.gemspec
@@ -19,6 +19,7 @@ Gem::Specification.new do |s|
   s.add_dependency "rails", "~> 3.2.13"
 
   s.add_development_dependency "sqlite3"
+  s.add_development_dependency "colored"
   s.add_development_dependency "rspec-rails", "~> 2.0"
   s.add_development_dependency "capybara"
 end
--- spec/features/js_spec.rb
+++ spec/features/js_spec.rb
@@ -1,4 +1,5 @@
 require 'spec_helper'
+require 'colored'
 
 feature "UI tests" do
 
@@ -7,6 +8,18 @@ feature "UI tests" do
 
         using_wait_time 300 do
             page.should have_selector('.result')
+        end
+
+        if page.find('.result .failed').text.to_i > 0
+            puts "Some unit tests failed.".red
+
+            STOP_ON_FAILING_TEST = "STOP_ON_FAILING_TEST"
+            if ENV[STOP_ON_FAILING_TEST]
+                puts "Press <RETURN> to terminate tests...".blue
+                STDIN.getc
+            else
+                puts "HINT: Set #{STOP_ON_FAILING_TEST} environment variable to keep browser opened.".red
+            end
         end
 
         within '.result' do
