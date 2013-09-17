bb9f93c2192eed89492ddb1c8420c0350df98583
Mike Andrzejewski <mike.aski@gatemedia.ch>
2013-09-17 14:33:37 +0200

    Use Ember.Inflector for plural/singular-ization

--- app/assets/javascripts/gatemedia/data/string.js
+++ app/assets/javascripts/gatemedia/data/string.js
@@ -8,15 +8,18 @@ String.prototype.endsWith = function (suffix) {
 };
 
 
+var inflector = new Ember.Inflector();
+inflector.plural(/$/, 's');
+inflector.plural(/y$/, 'ies');
+
+inflector.singular(/s$/,'');
+inflector.singular(/ies$/,'y');
+
 String.prototype.pluralize = function () {
-  return this + 's';
+  return inflector.pluralize(this);
 };
 
 
 String.prototype.singularize = function () {
-  var match = this.match(/(.+)s$/);
-  if (match) {
-    return match[1];
-  }
-  return this;
+  return inflector.singularize(this);
 };
