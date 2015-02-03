8b4042a696143a1fcfc755dd8fe5780b6761fdaa
Mike Andrzejewski <mike.aski@gatemedia.ch>
2015-02-03 08:57:09 +0100

    Bump ember-cli

--- tests/test-helper.js
+++ tests/test-helper.js
@@ -6,12 +6,6 @@ import {
 
 setResolver(resolver);
 
-document.write('<div id="ember-testing-container"><div id="ember-testing"></div></div>');
-
-QUnit.config.urlConfig.push({ id: 'nocontainer', label: 'Hide container'});
-var containerVisibility = QUnit.urlParams.nocontainer ? 'hidden' : 'visible';
-document.getElementById('ember-testing-container').style.visibility = containerVisibility;
-
 
 //TODO: remove when dependence working...
 
