d812030f48168f7096b8c9946f2599eb89c14b12
Mike Andrzejewski <mike.aski@gmail.com>
2015-04-08 14:16:02 +0200

    Bump revision

---
+++ config/ember-try.js
@@ -0,0 +1,17 @@
+'use strict';
+
+module.exports = {
+  scenarios: [
+    {
+      name: "Ember 1.11.1",
+      dependencies: {
+        "ember": "1.11.1"
+      }
+    }, {
+      name: "Ember 1.12.0-beta.1",
+      dependencies: {
+        "ember": "1.12.0-beta.1"
+      }
+    }
+  ]
+};
